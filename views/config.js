95a6becfeab7d72015d08f1385dcb505d6b41c64
Emma Lee <jy.lee.emma@gmail.com>
2023-05-25 13:45:00 -0700

    Remove config.js from views folder.

--- views/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user: 'bcitdtc11@gmail.com',
-    clientId: '447061527175-2aqtmq0u14g8aje4kiu5tuafv4b3daq1.apps.googleusercontent.com',
-    clientSecret: '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',
-    refreshToken: '1//04U9ae64tofDCCgYIARAAGAQSNwF-L9IrFzaT9UwsAI1bvllf_gW3rSInmpzWf-Zc0YzbRycT_zCXy3aKGRsORVGFx2CGUJ1eMmY'
-}
